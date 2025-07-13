97281c1c8b497255dfc7a89ac7e88ec220c6f88d
Mujojo03 <rosejaneforyou@gmail.com>
2025-07-13 19:52:08 +0300

    Empty repository

--- mini-app-01.create-curations/src/Content/Relays.js
+++
@@ -1,8 +0,0 @@
-const relaysOnPlatform = [
-  "wss://nostr-01.yakihonne.com",
-  "wss://nostr-02.yakihonne.com",
-  "wss://relay.damus.io",
-  "wss://relay.nostr.band",
-];
-
-export default relaysOnPlatform;
--- mini-app-01.create-curations/src/Store/Store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { NostrAuthorsReducer, ToastReducer } from "./Slides/StoreSlides";
-
-export const store = configureStore({
-  reducer: {
-    toast: ToastReducer,
-    nostrAuthors: NostrAuthorsReducer,
-  },
-});
